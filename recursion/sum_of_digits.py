d078db5b8d5b40b741eb59e66586ce9d672af3c7
Ajith Sundararaj <simply.ajith@gmail.com>
2020-05-19 08:27:15 +0530

    Add files via upload

--- recursion/sum_of_digits.py
+++ recursion/sum_of_digits.py
@@ -17,6 +17,15 @@ def sum_of_digit(num,total):
         num = num // 10
         return sum_of_digit(num,total)
 
+
+def sum_of_digit_1(num,total):
+    if num == 0:
+        return total
+    else:
+        total = total + num % 10
+        num = num // 10
+        return sum_of_digit_1(num,total)
+
 print(sum_of_digit(1,0))
 
 
